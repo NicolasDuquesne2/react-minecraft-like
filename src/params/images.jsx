63372c708b643a7b917a38bb6736f68741dc11d9
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2022-10-09 15:43:32 +0200

    put images and set an images object

---
+++ src/params/images.jsx
@@ -0,0 +1,16 @@
+import dirtImg from '../assets/dirt.jpg'
+import grassImg from '../assets/grass.jpg'
+import glassImg from '../assets/glass.png'
+import logImg from '../assets/log.jpg'
+import woodImg from '../assets/wood.png'
+
+const images = {
+    dirtImg,
+    grassImg,
+    glassImg,
+    logImg,
+    woodImg
+}
+
+
+export default images
